ba17a9006fa19580a1a47ef46115ef6d710c8860
Sauli Anto <sauli.anto@gofore.com>
2024-02-19 13:04:33 +0200

    OPHJOD-203: Add development IdP and OIDC log in

---
+++ src/main/java/fi/okm/jod/yksilo/controller/PrincipalController.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2024 The Finnish Ministry of Education and Culture, The Finnish
+ * The Ministry of Economic Affairs and Employment, The Finnish National Agency of
+ * Education (Opetushallitus) and The Finnish Development and Administration centre
+ * for ELY Centres and TE Offices (KEHA).
+ *
+ * Licensed under the EUPL-1.2-or-later.
+ */
+
+package fi.okm.jod.yksilo.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/principal")
+public class PrincipalController {
+
+  @GetMapping
+  public Object getPrincipal(Authentication authentication) {
+    return authentication.getPrincipal();
+  }
+}
