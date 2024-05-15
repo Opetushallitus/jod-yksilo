b8166f29f78dc4757428bc626ca08e7fca541e52
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2024-05-15 11:15:57 +0300

    OPHJOD-410: Fix location header uses wrong scheme

--- src/main/java/fi/okm/jod/yksilo/Application.java
+++ src/main/java/fi/okm/jod/yksilo/Application.java
@@ -11,9 +11,11 @@ package fi.okm.jod.yksilo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 /** Application entrypoint. */
 @SpringBootApplication
+@ConfigurationPropertiesScan("fi.okm.jod.yksilo.config")
 public class Application {
 
   public static void main(String[] args) {
