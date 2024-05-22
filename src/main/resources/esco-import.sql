4569c354dfff8fd3da99dac2d24d29430c52fb8a
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2024-05-22 12:33:55 +0300

    OPHJOD-432: Fix osaaminen_seq does not exist

--- src/main/resources/esco-import.sql
+++ src/main/resources/esco-import.sql
@@ -1,3 +1,6 @@
+CREATE SEQUENCE IF NOT EXISTS osaaminen_seq;
+SELECT setval('osaaminen_seq', (SELECT COALESCE(MAX(id), 1) FROM osaaminen));
+
 INSERT INTO public.osaaminen (id, uri) SELECT nextval('osaaminen_seq'), "conceptUri"  AS uri FROM esco_data.skills_fi;
 INSERT INTO public.osaaminen_kaannos (osaaminen_id, kaannos_key,  kuvaus, nimi) SELECT o.id AS osaaminen_id, 'FI' AS kaannos_key, description AS kuvaus, "preferredLabel"  AS nimi FROM esco_data.skills_fi e JOIN public.osaaminen o ON o.uri = e."conceptUri";
 INSERT INTO public.osaaminen_kaannos (osaaminen_id, kaannos_key,  kuvaus, nimi) SELECT o.id AS osaaminen_id, 'SV' AS kaannos_key, description AS kuvaus, "preferredLabel"  AS nimi FROM esco_data.skills_sv e JOIN public.osaaminen o ON o.uri = e."conceptUri";
