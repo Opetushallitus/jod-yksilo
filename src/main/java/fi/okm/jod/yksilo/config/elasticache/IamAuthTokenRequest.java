a853eef5b048496cc8ba7fa6a3bb4cf1c2459330
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2024-08-02 13:54:35 +0300

    OPHJOD-551: Replace deprecated Aws4Signer

--- src/main/java/fi/okm/jod/yksilo/config/elasticache/IamAuthTokenRequest.java
+++ src/main/java/fi/okm/jod/yksilo/config/elasticache/IamAuthTokenRequest.java
@@ -9,30 +9,35 @@
 
 package fi.okm.jod.yksilo.config.elasticache;
 
+import java.time.Clock;
 import java.time.Duration;
-import java.time.Instant;
 import java.util.Collections;
+import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
-import software.amazon.awssdk.auth.signer.Aws4Signer;
-import software.amazon.awssdk.auth.signer.params.Aws4PresignerParams;
-import software.amazon.awssdk.http.SdkHttpFullRequest;
 import software.amazon.awssdk.http.SdkHttpMethod;
+import software.amazon.awssdk.http.SdkHttpRequest;
+import software.amazon.awssdk.http.auth.aws.signer.AwsV4FamilyHttpSigner.AuthLocation;
+import software.amazon.awssdk.http.auth.aws.signer.AwsV4HttpSigner;
 import software.amazon.awssdk.regions.Region;
 
+@Slf4j
 public final class IamAuthTokenRequest {
-  private static final Duration TOKEN_EXPIRY_DURATION_SECONDS = Duration.ofSeconds(900);
+  private static final Duration TOKEN_EXPIRY_DURATION = Duration.ofSeconds(900);
 
-  private final SdkHttpFullRequest request;
+  private final SdkHttpRequest request;
   private final Region region;
+  private final Clock clock;
 
-  // Suppress deprecation warnings for the Aws4Signer class because it is same implementation as
-  // in the software.amazon.awssdk.services.rds.DefaultRdsUtilities.generateAuthenticationToken.
-  @SuppressWarnings("deprecation")
-  private final software.amazon.awssdk.auth.signer.Aws4Signer signer = Aws4Signer.create();
+  private final AwsV4HttpSigner signer = AwsV4HttpSigner.create();
 
   public IamAuthTokenRequest(String userId, String cacheName, Region region) {
+    this(userId, cacheName, region, Clock.systemUTC());
+  }
+
+  // for testing
+  IamAuthTokenRequest(String userId, String cacheName, Region region, Clock clock) {
     this.request =
-        SdkHttpFullRequest.builder()
+        SdkHttpRequest.builder()
             .method(SdkHttpMethod.GET)
             .protocol("https")
             .host(cacheName)
@@ -42,21 +47,31 @@ public final class IamAuthTokenRequest {
             .putRawQueryParameter("ResourceType", Collections.singletonList("ServerlessCache"))
             .build();
     this.region = region;
+    this.clock = clock;
   }
 
   public String toSignedRequestUri(AwsCredentials credentials) {
-    return sign(request, credentials).getUri().toString().replace("https://", "");
+    return sign(credentials).getUri().toString().replace("https://", "");
   }
 
-  private SdkHttpFullRequest sign(SdkHttpFullRequest request, AwsCredentials credentials) {
-    Instant expiryInstant = Instant.now().plus(TOKEN_EXPIRY_DURATION_SECONDS);
-    Aws4PresignerParams signerParams =
-        Aws4PresignerParams.builder()
-            .signingRegion(region)
-            .awsCredentials(credentials)
-            .signingName("elasticache")
-            .expirationTime(expiryInstant)
-            .build();
-    return signer.presign(request, signerParams);
+  @SuppressWarnings("java:S3252")
+  private SdkHttpRequest sign(AwsCredentials credentials) {
+    return signer
+        .sign(
+            r ->
+                r.identity(credentials)
+                    .request(request)
+                    .putProperty(AwsV4HttpSigner.SIGNING_CLOCK, clock)
+                    .putProperty(AwsV4HttpSigner.REGION_NAME, region.toString())
+                    .putProperty(AwsV4HttpSigner.SERVICE_SIGNING_NAME, "elasticache")
+                    // presigned-->
+                    //   - expiration duration must be set
+                    //   - auth location must be set to query string
+                    //   - payload signing must be enabled and the payload must be null (no payload)
+                    .putProperty(AwsV4HttpSigner.PAYLOAD_SIGNING_ENABLED, true)
+                    .putProperty(AwsV4HttpSigner.EXPIRATION_DURATION, TOKEN_EXPIRY_DURATION)
+                    .putProperty(AwsV4HttpSigner.AUTH_LOCATION, AuthLocation.QUERY_STRING)
+                    .build())
+        .request();
   }
 }
--- src/test/java/fi/okm/jod/yksilo/config/elasticache/IamAuthTokenRequestTest.java
+++ src/test/java/fi/okm/jod/yksilo/config/elasticache/IamAuthTokenRequestTest.java
@@ -9,25 +9,37 @@
 
 package fi.okm.jod.yksilo.config.elasticache;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.regions.Region;
 
+@Slf4j
 class IamAuthTokenRequestTest {
 
   private static final String USERNAME = "user123";
   private static final String CACHE_NAME = "my-cache";
+  private static final Clock clock =
+      Clock.fixed(Instant.parse("2024-08-02T12:11:50Z"), ZoneOffset.UTC);
 
   @Test
   void testToSignedRequestUri() {
     var testAwsCredentials = AwsBasicCredentials.create("accessKey", "secretKey");
     String expectedUriPrefix =
         CACHE_NAME + "/?Action=connect&User=" + USERNAME + "&ResourceType=ServerlessCache";
-    IamAuthTokenRequest request = new IamAuthTokenRequest(USERNAME, CACHE_NAME, Region.EU_WEST_1);
+    IamAuthTokenRequest request =
+        new IamAuthTokenRequest(USERNAME, CACHE_NAME, Region.EU_WEST_1, clock);
     String signedUri = request.toSignedRequestUri(testAwsCredentials);
-    assertTrue(signedUri.startsWith(expectedUriPrefix));
-    assertTrue(signedUri.contains("X-Amz-Signature"));
+
+    log.info("Signed URI: {}", signedUri);
+    assertThat(signedUri)
+        .startsWith(expectedUriPrefix)
+        .contains(
+            "X-Amz-Signature=5dda5db919b1337db6596b2a608924bc10197fb39289b933b60bdb114d5a70dc");
   }
 }
