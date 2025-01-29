3d5e413003ea2b2aff013a575f3cd45f1530ba85
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2025-01-29 12:45:01 +0200

    OPHJOD-1221: Refactor init-local-env

--- scripts/init-local-env.sh
+++ scripts/init-local-env.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e -o pipefail
 
+mkdir -p ./tmp/data
+mkdir -p ./.run
+
 if [[ -n $AWS_SESSION_TOKEN && -n $DEV_BUCKET ]]; then
-  mkdir -p ./tmp/data
-  mkdir -p ./.run
   aws s3 cp s3://${DEV_BUCKET}/jod-yksilo-backend/application-local.yml .
   aws s3 cp s3://${DEV_BUCKET}/jod-yksilo-backend/jod-yksilo-bootRun.run.xml .run/
   aws s3 sync "s3://${DEV_BUCKET}/data/jod-yksilo-esco-data/" ./tmp/data/ --exclude "*" --include "*.sql" --include "*.csv"
@@ -22,42 +23,46 @@ if [[ -z $DB ]]; then
   STARTED=true
 fi
 
+echo -e "NOTE: Creating database schema. Errors about 'relation X does not exist' can be ignored.\n"
+{ echo '\set VERBOSITY terse'; cat ./src/main/resources/schema.sql; } \
+     | docker exec -i -e PGPASSWORD=yksilo -e PGOPTIONS='--client-min-messages=warning' "$DB" psql -q -f - -U yksilo yksilo
+echo "NOTE: Importing data"
 (
   cd ./tmp/data
-  cat esco_data_scheme_2.sql\
-   | docker exec -i -e PGPASSWORD=yksilo "$DB" psql -q -f - -U yksilo yksilo
+
+  docker exec -i -e PGPASSWORD=yksilo "$DB" psql -q -1 -U yksilo yksilo \
+    -c "TRUNCATE TABLE esco_data.skills"
 
   for lang in "en" "fi" "sv"; do
     <skills_${lang}.csv docker exec \
-      -i -e PGPASSWORD=yksilo "$DB" psql -q -U yksilo yksilo \
+      -i -e PGPASSWORD=yksilo "$DB" psql -q -1 -U yksilo yksilo \
       -c "SET esco.lang=${lang}" \
       -c "\COPY esco_data.skills(conceptType,conceptUri,skillType,reuseLevel,preferredLabel,altLabels,hiddenLabels,status,modifiedDate,scopeNote,definition,inScheme,description) FROM STDIN (FORMAT CSV, HEADER true)"
   done
 
+  docker exec -i -e PGPASSWORD=yksilo "$DB" psql -q -1 -U yksilo yksilo \
+      -c "TRUNCATE TABLE esco_data.occupations"
+
   for lang in "en" "fi" "sv"; do
     <occupations_${lang}.csv docker exec \
-      -i -e PGPASSWORD=yksilo "$DB" psql -q -U yksilo yksilo \
+      -i -e PGPASSWORD=yksilo "$DB" psql -q -1 -U yksilo yksilo \
       -c "SET esco.lang=${lang}" \
       -c "\COPY esco_data.occupations(conceptType,conceptUri,iscoGroup,preferredLabel,altLabels,hiddenLabels,status,modifiedDate,regulatedProfessionNote,scopeNote,definition,inScheme,description,code) FROM STDIN (FORMAT CSV, HEADER true)"
   done
 
   for lang in "en" "fi" "sv"; do
     <ISCOGroups_${lang}.csv docker exec \
-      -i -e PGPASSWORD=yksilo "$DB" psql -q -U yksilo yksilo \
+      -i -e PGPASSWORD=yksilo "$DB" psql -q -1 -U yksilo yksilo \
       -c "SET esco.lang=${lang}"\
       -c "\COPY esco_data.occupations(conceptType,conceptUri,code,preferredLabel,status,altLabels,inScheme,description) FROM STDIN (FORMAT csv, HEADER true);"
   done
 
   for mahdollisuus in "tyomahdollisuus" "koulutusmahdollisuus"; do
-    echo "\
-    CREATE SCHEMA IF NOT EXISTS ${mahdollisuus}_data;
-    CREATE TABLE IF NOT EXISTS ${mahdollisuus}_data.import(
-      data jsonb not null,
-      id uuid generated always as ( (data ->> 'id')::uuid ) stored primary key);
-    TRUNCATE TABLE ${mahdollisuus}_data.import;"\
-     | docker exec -i -e PGPASSWORD=yksilo "$DB" psql -q -1 -f - -U yksilo yksilo
+    docker exec -i -e PGPASSWORD=yksilo "$DB" psql -q -1 -U yksilo yksilo \
+    -c "TRUNCATE TABLE ${mahdollisuus}_data.import"
+
     <full_json_lines_${mahdollisuus}.json docker exec \
-      -i -e PGPASSWORD=yksilo "$DB" psql -q -U yksilo yksilo \
+      -i -e PGPASSWORD=yksilo "$DB" psql -q -1 -U yksilo yksilo \
       -c "\COPY ${mahdollisuus}_data.import(data) FROM STDIN (FORMAT text)"
   done
 )
