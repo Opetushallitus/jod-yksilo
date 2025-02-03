0550599b901d4a9812b9e2fff077f25a694ee51a
NhutDo <nhut.do@elenium.fi>
2025-02-03 15:37:31 +0200

    OPHJOD-1217: Implement OAuth2 client configuration and controller for Koski integration.

---
+++ src/main/java/fi/okm/jod/yksilo/config/koski/KoskiOAuth2Config.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2024 The Finnish Ministry of Education and Culture, The Finnish
+ * The Ministry of Economic Affairs and Employment, The Finnish National Agency of
+ * Education (Opetushallitus) and The Finnish Development and Administration centre
+ * for ELY Centres and TE Offices (KEHA).
+ *
+ * Licensed under the EUPL-1.2-or-later.
+ */
+
+package fi.okm.jod.yksilo.config.koski;
+
+import java.time.Duration;
+import java.util.List;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
+import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
+import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
+import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
+import org.springframework.web.client.RestClient;
+
+@Getter
+@Slf4j
+@ConditionalOnProperty(
+    name = "spring.security.oauth2.client.registration.koski.provider",
+    havingValue = "koski-mtls")
+@Configuration(proxyBeanMethods = false)
+public class KoskiOAuth2Config {
+
+  public static final String RESTCLIENT_ID = "koski-rest-client";
+  private static final String REGISTRATION_ID = "koski";
+  private static final String SSL_BUNDLE = "koski-ssl-bundle";
+
+  private final String resourceServer;
+
+  public KoskiOAuth2Config(@Value("${koski.resource-server.url}") String resourceServer) {
+    this.resourceServer = resourceServer;
+  }
+
+  @Bean(RESTCLIENT_ID)
+  public RestClient koskiRestClient(
+      RestClient.Builder builder,
+      OAuth2AuthorizedClientManager authorizedClientManager,
+      SslBundles sslBundles) {
+    var requestFactory =
+        ClientHttpRequestFactoryBuilder.jdk()
+            .build(
+                ClientHttpRequestFactorySettings.defaults()
+                    .withSslBundle(sslBundles.getBundle(SSL_BUNDLE))
+                    .withConnectTimeout(Duration.ofSeconds(5))
+                    .withReadTimeout(Duration.ofSeconds(10)));
+
+    var messageConverters =
+        List.of(
+            new FormHttpMessageConverter(), new OAuth2AccessTokenResponseHttpMessageConverter(),
+            new StringHttpMessageConverter(), new MappingJackson2HttpMessageConverter());
+
+    return builder
+        .requestInterceptor(new OAuth2ClientHttpRequestInterceptor(authorizedClientManager))
+        .requestFactory(requestFactory)
+        .messageConverters(messageConverters)
+        .defaultStatusHandler(new OAuth2ErrorResponseErrorHandler())
+        .build();
+  }
+
+  public String getRegistrationId() {
+    return REGISTRATION_ID;
+  }
+}
---
+++ src/main/java/fi/okm/jod/yksilo/config/koski/KoskiSecurityConfig.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2024 The Finnish Ministry of Education and Culture, The Finnish
+ * The Ministry of Economic Affairs and Employment, The Finnish National Agency of
+ * Education (Opetushallitus) and The Finnish Development and Administration centre
+ * for ELY Centres and TE Offices (KEHA).
+ *
+ * Licensed under the EUPL-1.2-or-later.
+ */
+
+package fi.okm.jod.yksilo.config.koski;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.endpoint.RestClientAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestClient;
+
+@ConditionalOnBean(KoskiOAuth2Config.class)
+@Configuration(KoskiSecurityConfig.SECURITY_CONFIG)
+public class KoskiSecurityConfig {
+
+  public static final String SECURITY_CONFIG = "koski-security-config";
+
+  @Bean
+  public SecurityFilterChain oauthConfig(
+      HttpSecurity http,
+      ClientRegistrationRepository repository,
+      @Qualifier(KoskiOAuth2Config.RESTCLIENT_ID) RestClient oauthRestClient,
+      HttpSessionOAuth2AuthorizedClientRepository authorizedClientRepository)
+      throws Exception {
+    return http.csrf(
+            csrf -> csrf.ignoringRequestMatchers(request -> request.getSession(false) != null))
+        .securityMatcher(
+            "/error",
+            "/oauth2/authorize/koski", // For front end to call.
+            "/oauth2/authorization/koski",
+            "/oauth2/logout/koski",
+            "/oauth2/response/koski" // OAuth2 callback URL
+            )
+        .oauth2Client(
+            client -> {
+              client.authorizationCodeGrant(
+                  code -> {
+                    code.authorizationRequestResolver(
+                        createKoskiAuthorizationRequestResolver(repository));
+                    code.accessTokenResponseClient(
+                        createAccessTokenResponseClient(oauthRestClient));
+                  });
+              client.authorizedClientRepository(authorizedClientRepository);
+            })
+        .build();
+  }
+
+  private static RestClientAuthorizationCodeTokenResponseClient createAccessTokenResponseClient(
+      RestClient oauthRestClient) {
+    var responseClient = new RestClientAuthorizationCodeTokenResponseClient();
+    responseClient.setRestClient(oauthRestClient);
+    return responseClient;
+  }
+
+  private static DefaultOAuth2AuthorizationRequestResolver createKoskiAuthorizationRequestResolver(
+      ClientRegistrationRepository repository) {
+    var resolver =
+        new DefaultOAuth2AuthorizationRequestResolver(
+            repository,
+            OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI);
+    resolver.setAuthorizationRequestCustomizer(
+        OAuth2AuthorizationRequestCustomizers.withPkce()
+            .andThen(
+                customizer ->
+                    customizer.additionalParameters(
+                        additionalParameters ->
+                            additionalParameters.put("response_mode", "form_post"))));
+    return resolver;
+  }
+
+  @Bean
+  public HttpSessionOAuth2AuthorizedClientRepository authorizedClientRepository() {
+    return new HttpSessionOAuth2AuthorizedClientRepository();
+  }
+}
