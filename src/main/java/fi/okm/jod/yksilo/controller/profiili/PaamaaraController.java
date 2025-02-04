321815e49fea5155be6af1d3112c901d8ca1bcc6
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2025-02-04 10:28:34 +0200

    OPHJOD-1219: Fix validation

--- src/main/java/fi/okm/jod/yksilo/controller/profiili/PaamaaraController.java
+++ src/main/java/fi/okm/jod/yksilo/controller/profiili/PaamaaraController.java
@@ -11,13 +11,16 @@ package fi.okm.jod.yksilo.controller.profiili;
 
 import fi.okm.jod.yksilo.domain.JodUser;
 import fi.okm.jod.yksilo.dto.profiili.PaamaaraDto;
+import fi.okm.jod.yksilo.dto.validationgroup.Add;
 import fi.okm.jod.yksilo.service.profiili.PaamaaraService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,14 +45,17 @@ class PaamaaraController {
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  UUID add(@AuthenticationPrincipal JodUser user, @RequestBody PaamaaraDto dto) {
+  UUID add(
+      @AuthenticationPrincipal JodUser user, @Validated(Add.class) @RequestBody PaamaaraDto dto) {
     return service.add(user, dto);
   }
 
   @PutMapping("/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   void update(
-      @AuthenticationPrincipal JodUser user, @PathVariable UUID id, @RequestBody PaamaaraDto dto) {
+      @AuthenticationPrincipal JodUser user,
+      @PathVariable UUID id,
+      @Valid @RequestBody PaamaaraDto dto) {
     if (dto.id() == null || !id.equals(dto.id())) {
       throw new IllegalArgumentException("Invalid identifier");
     }
