619c26bb7b147d00f120948111dc7cf05c563646
NhutDo <nhut.do@elenium.fi>
2025-02-05 14:59:58 +0200

    OPHJOD-1217: Add new API endpoint GET /api/integraatiot/koski/koulutukset to get user's educations.

--- src/main/java/fi/okm/jod/yksilo/controller/IntegraatioController.java
+++ src/main/java/fi/okm/jod/yksilo/controller/IntegraatioController.java
@@ -9,23 +9,32 @@
 
 package fi.okm.jod.yksilo.controller;
 
+import fi.okm.jod.yksilo.config.koski.KoskiOAuth2Service;
 import fi.okm.jod.yksilo.dto.profiili.KoulutusDto;
 import fi.okm.jod.yksilo.service.KoskiService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/integraatiot")
 @RequiredArgsConstructor
 @Tag(name = "integraatiot")
 class IntegraatioController {
+
+  private final KoskiOAuth2Service koskiOAuth2Service;
   private final KoskiService koskiService;
 
   // Define a set of allowed base URLs or patterns
@@ -48,5 +57,18 @@ class IntegraatioController {
             + uri.getHost()
             + uri.getPath().replaceFirst("koski/opinnot", "koski/api/opinnot");
     return URI.create(koskiApiUri);
+  }
+
+  @GetMapping("/koski/koulutukset")
+  @Operation(summary = "Get user's educations from Koski.")
+  ResponseEntity<List<KoulutusDto>> getEducationsDataFromKoski(
+      Authentication authentication, HttpServletRequest request) {
+    log.debug("Getting koulutukset...");
+    var authorizedClient = koskiOAuth2Service.getAuthorizedClient(authentication, request);
+    if (authorizedClient == null) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+    var dataInJson = koskiOAuth2Service.fetchDataFromResourceServer(authorizedClient);
+    return ResponseEntity.ok(koskiService.getKoulutusData(dataInJson, null));
   }
 }
