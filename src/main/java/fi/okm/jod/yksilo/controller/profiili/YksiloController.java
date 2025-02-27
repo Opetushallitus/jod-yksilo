7ebf97f629de58826de634d237c2ba7fbbc3c60e
Sauli Anto <sauli.anto@gofore.com>
2025-02-27 12:07:23 +0200

    OPHJOD-1131: Add tervetuloapolku field

--- src/main/java/fi/okm/jod/yksilo/controller/profiili/YksiloController.java
+++ src/main/java/fi/okm/jod/yksilo/controller/profiili/YksiloController.java
@@ -10,19 +10,22 @@
 package fi.okm.jod.yksilo.controller.profiili;
 
 import fi.okm.jod.yksilo.domain.JodUser;
-import fi.okm.jod.yksilo.dto.CsrfTokenDto;
-import fi.okm.jod.yksilo.dto.profiili.YksiloCsrfDto;
+import fi.okm.jod.yksilo.dto.profiili.YksiloDto;
 import fi.okm.jod.yksilo.service.profiili.YksiloService;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,14 +44,26 @@ public class YksiloController {
         user.givenName(),
         user.familyName(),
         new CsrfTokenDto(
-            csrfToken.getToken(), csrfToken.getHeaderName(), csrfToken.getParameterName()));
+            csrfToken.getToken(), csrfToken.getHeaderName(), csrfToken.getParameterName()),
+        yksiloService.get(user).tervetuloapolku());
+  }
+
+  @PutMapping
+  public void update(@AuthenticationPrincipal JodUser user, @RequestBody @Valid YksiloDto dto) {
+    yksiloService.update(user, dto);
   }
 
   @DeleteMapping
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void delete(HttpServletRequest request, @AuthenticationPrincipal JodUser user)
       throws ServletException {
-    yksiloService.deleteYksilo(user);
+    yksiloService.delete(user);
     request.logout();
   }
+
+  public record YksiloCsrfDto(
+      String etunimi, String sukunimi, @NotNull CsrfTokenDto csrf, boolean tervetuloapolku) {}
+
+  public record CsrfTokenDto(
+      @NotNull String token, @NotNull String headerName, @NotNull String parameterName) {}
 }
--- src/main/java/fi/okm/jod/yksilo/service/profiili/YksiloService.java
+++ src/main/java/fi/okm/jod/yksilo/service/profiili/YksiloService.java
@@ -10,7 +10,10 @@
 package fi.okm.jod.yksilo.service.profiili;
 
 import fi.okm.jod.yksilo.domain.JodUser;
+import fi.okm.jod.yksilo.dto.profiili.YksiloDto;
+import fi.okm.jod.yksilo.entity.Yksilo;
 import fi.okm.jod.yksilo.repository.YksiloRepository;
+import fi.okm.jod.yksilo.service.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,8 +24,25 @@ import org.springframework.transaction.annotation.Transactional;
 public class YksiloService {
   private final YksiloRepository yksilot;
 
-  public void deleteYksilo(JodUser user) {
+  public YksiloDto get(JodUser user) {
+    var yksilo = getYksilo(user);
+    return new YksiloDto(yksilo.getTervetuloapolku());
+  }
+
+  public void update(JodUser user, YksiloDto dto) {
+    var yksilo = getYksilo(user);
+    yksilo.setTervetuloapolku(dto.tervetuloapolku());
+    yksilot.save(yksilo);
+  }
+
+  public void delete(JodUser user) {
     yksilot.deleteById(user.getId());
     yksilot.removeId(user.getId());
+  }
+
+  private Yksilo getYksilo(JodUser user) {
+    return yksilot
+        .findById(user.getId())
+        .orElseThrow(() -> new NotFoundException("Profiili does not exist"));
   }
 }
