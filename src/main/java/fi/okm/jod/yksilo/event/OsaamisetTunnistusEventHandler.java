44c9730f1bec8e8ef5ffd6fa2c062aed8605d834
Nhut Do <nhut.do@elenium.fi>
2025-03-20 17:50:14 +0200

    OPHJOD-1310: Temporary set Osaamiset tunnistus to fail

--- src/main/java/fi/okm/jod/yksilo/event/OsaamisetTunnistusEventHandler.java
+++ src/main/java/fi/okm/jod/yksilo/event/OsaamisetTunnistusEventHandler.java
@@ -9,26 +9,46 @@
 
 package fi.okm.jod.yksilo.event;
 
+import fi.okm.jod.yksilo.entity.OsaamisenTunnistusStatus;
+import fi.okm.jod.yksilo.repository.KoulutusRepository;
+import jakarta.servlet.UnavailableException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class OsaamisetTunnistusEventHandler {
+
+  private final KoulutusRepository koulutusRepository;
 
   @EventListener
   @Async
   public void handleOsaamisetTunnistusEvent(OsaamisetTunnistusEvent event) {
     try {
       log.debug("Osaamiset tunnistus event: {}", event);
-      // Fetch entity from database
       // Make request to AI API
       // Update entity with response data.
+      Thread.sleep(5_000);
+      throw new UnavailableException("Not yet implemented: Osaamiset tunnistus");
 
     } catch (Exception e) {
       log.error("Error processing OsaamisetTunnistusEvent: {}", e.getMessage(), e);
+      updateOsaamisetTunnistusStatus(event, OsaamisenTunnistusStatus.FAIL);
+    }
+  }
+
+  private void updateOsaamisetTunnistusStatus(
+      OsaamisetTunnistusEvent event, OsaamisenTunnistusStatus newStatus) {
+    for (var koulutusKokonaisuus : event.koulutusKokonaisuusList()) {
+      var koulutukset = koulutusKokonaisuus.getKoulutukset();
+      for (var koulutus : koulutukset) {
+        koulutus.setOsaamisenTunnistusStatus(newStatus);
+      }
+      koulutusRepository.saveAllAndFlush(koulutukset);
     }
   }
 }
