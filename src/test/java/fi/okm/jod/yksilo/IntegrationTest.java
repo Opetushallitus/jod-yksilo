bc2f516c46dacc012ed9adecde0a849a3a5f5d2d
Nhut Do <nhut.do@elenium.fi>
2025-03-27 13:05:28 +0200

    OPHJOD-1310: Remove comment

--- src/test/java/fi/okm/jod/yksilo/IntegrationTest.java
+++ src/test/java/fi/okm/jod/yksilo/IntegrationTest.java
@@ -13,19 +13,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-/**
- * Add the following code to the implemented class as we want integration test classes to run own
- * test container instance not to share with another processes as it will cause issues when running
- * in parallel:
- *
- * {@snippet :
- * @Container @ServiceConnection
- * private static final PostgreSQLContainer<?> POSTGRES_CONTAINER = TestUtil.createPostgresSQLContainer();
- *
- * @Container @ServiceConnection
- * private static final GenericContainer<?> REDIS_CONTAINER = TestUtil.createRedisContainer();
- * }
- */
 @Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
