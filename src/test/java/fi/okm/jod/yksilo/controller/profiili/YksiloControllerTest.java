8785b4f5931f7572849eb14e1d4d2fb3a53fff21
Sauli Anto <sauli.anto@gofore.com>
2025-03-28 11:46:26 +0200

    OPHJOD-1395: Add paamaarat relationship to Yksilo entity

--- src/test/java/fi/okm/jod/yksilo/controller/profiili/YksiloControllerTest.java
+++ src/test/java/fi/okm/jod/yksilo/controller/profiili/YksiloControllerTest.java
@@ -12,6 +12,7 @@ package fi.okm.jod.yksilo.controller.profiili;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -83,5 +84,12 @@ class YksiloControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(dto)))
         .andExpect(status().isOk());
+  }
+
+  @Test
+  @WithUserDetails("test")
+  void shouldDeleteUserInformation() throws Exception {
+    mockMvc.perform(delete("/api/profiili/yksilo").with(csrf())).andExpect(status().isNoContent());
+    mockMvc.perform(get("/api/profiili/yksilo").with(csrf())).andExpect(status().isUnauthorized());
   }
 }
---
+++ src/test/java/fi/okm/jod/yksilo/service/profiili/YksiloServiceTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2025 The Finnish Ministry of Education and Culture, The Finnish
+ * The Ministry of Economic Affairs and Employment, The Finnish National Agency of
+ * Education (Opetushallitus) and The Finnish Development and Administration centre
+ * for ELY Centres and TE Offices (KEHA).
+ *
+ * Licensed under the EUPL-1.2-or-later.
+ */
+
+package fi.okm.jod.yksilo.service.profiili;
+
+import static fi.okm.jod.yksilo.testutil.LocalizedStrings.ls;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import fi.okm.jod.yksilo.domain.MahdollisuusTyyppi;
+import fi.okm.jod.yksilo.domain.PaamaaraTyyppi;
+import fi.okm.jod.yksilo.dto.profiili.PaamaaraDto;
+import fi.okm.jod.yksilo.repository.TyomahdollisuusRepository;
+import fi.okm.jod.yksilo.service.AbstractServiceTest;
+import fi.okm.jod.yksilo.service.NotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+
+@Sql(
+    scripts = {"/data/mock-tunnistus.sql"},
+    config = @SqlConfig(separator = ";;;"))
+@Sql(scripts = {"/data/mahdollisuudet-test-data.sql"})
+@Import({YksiloService.class, PaamaaraService.class})
+class YksiloServiceTest extends AbstractServiceTest {
+  @Autowired private YksiloService service;
+  @Autowired private PaamaaraService paamaaraService;
+  @Autowired private TyomahdollisuusRepository tyomahdollisuudet;
+
+  @Test
+  void delete() {
+    paamaaraService.add(
+        user,
+        new PaamaaraDto(
+            null,
+            PaamaaraTyyppi.MUU,
+            MahdollisuusTyyppi.TYOMAHDOLLISUUS,
+            tyomahdollisuudet.findAll().getFirst().getId(),
+            ls("tavoite"),
+            null,
+            null));
+    simulateCommit();
+    assertDoesNotThrow(() -> service.delete(user));
+    assertEquals(0, paamaaraService.findAll(user).size());
+    assertThrows(NotFoundException.class, () -> service.get(user));
+  }
+}
