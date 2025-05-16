d9d93c79a586f6ba8687abd52ab2425dbcf09edc
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2025-05-16 08:50:53 +0300

    OPHJOD-1613: Fix bug in suggestion query. Refactor.

--- src/main/java/fi/okm/jod/yksilo/repository/TyomahdollisuusRepository.java
+++ src/main/java/fi/okm/jod/yksilo/repository/TyomahdollisuusRepository.java
@@ -10,13 +10,7 @@
 package fi.okm.jod.yksilo.repository;
 
 import fi.okm.jod.yksilo.entity.tyomahdollisuus.Tyomahdollisuus;
-import java.util.SequencedSet;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-public interface TyomahdollisuusRepository extends JpaRepository<Tyomahdollisuus, UUID> {
-
-  @Query("SELECT t.id FROM Tyomahdollisuus t ORDER BY t.id")
-  SequencedSet<UUID> fetchAllIds();
-}
+public interface TyomahdollisuusRepository extends JpaRepository<Tyomahdollisuus, UUID> {}
