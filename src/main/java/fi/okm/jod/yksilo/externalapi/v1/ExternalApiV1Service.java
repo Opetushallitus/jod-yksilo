7b80e2953f3edc8a68575afcb911b4e13e90e386
Lauri Holopainen <lauri.holopainen@elenium.fi>
2025-06-26 13:34:41 +0300

    OPHJOD-1697: created endpoint for tyomahdollisuudet

---
+++ src/main/java/fi/okm/jod/yksilo/externalapi/v1/ExternalApiV1Service.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2025 The Finnish Ministry of Education and Culture, The Finnish
+ * The Ministry of Economic Affairs and Employment, The Finnish National Agency of
+ * Education (Opetushallitus) and The Finnish Development and Administration centre
+ * for ELY Centres and TE Offices (KEHA).
+ *
+ * Licensed under the EUPL-1.2-or-later.
+ */
+
+package fi.okm.jod.yksilo.externalapi.v1;
+
+import fi.okm.jod.yksilo.dto.SivuDto;
+import fi.okm.jod.yksilo.entity.tyomahdollisuus.Tyomahdollisuus;
+import fi.okm.jod.yksilo.externalapi.v1.dto.ExtTyoMahdollisuusDto;
+import fi.okm.jod.yksilo.externalapi.v1.dto.ExtTyomahdollisuusMapper;
+import fi.okm.jod.yksilo.repository.TyomahdollisuusRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/** Bisneslogiikka External API V1 */
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ExternalApiV1Service {
+  private final TyomahdollisuusRepository tyomahdollisuusRepository;
+
+  @Transactional
+  public SivuDto<ExtTyoMahdollisuusDto> findTyoMahdollisuudet(final Pageable pageable) {
+    final Page<Tyomahdollisuus> tyomahdollisuusPage =
+        this.tyomahdollisuusRepository.findAll(pageable);
+    final List<ExtTyoMahdollisuusDto> tyoMahdollisuusDtoList =
+        tyomahdollisuusPage.stream().map(ExtTyomahdollisuusMapper::toTyoMahdollisuusDto).toList();
+    return new SivuDto<>(tyoMahdollisuusDtoList, pageable.getPageSize(), pageable.getPageNumber());
+  }
+}
---
+++ src/main/java/fi/okm/jod/yksilo/externalapi/v1/dto/ExtTyomahdollisuusMapper.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2025 The Finnish Ministry of Education and Culture, The Finnish
+ * The Ministry of Economic Affairs and Employment, The Finnish National Agency of
+ * Education (Opetushallitus) and The Finnish Development and Administration centre
+ * for ELY Centres and TE Offices (KEHA).
+ *
+ * Licensed under the EUPL-1.2-or-later.
+ */
+
+package fi.okm.jod.yksilo.externalapi.v1.dto;
+
+import fi.okm.jod.yksilo.entity.tyomahdollisuus.Tyomahdollisuus;
+
+/** */
+public class ExtTyomahdollisuusMapper {
+  private ExtTyomahdollisuusMapper() {}
+
+  public static ExtTyoMahdollisuusDto toTyoMahdollisuusDto(final Tyomahdollisuus tyomahdollisuus) {
+    return new ExtTyoMahdollisuusDto(
+        tyomahdollisuus.getId(),
+        tyomahdollisuus.getOtsikko(),
+        tyomahdollisuus.getKuvaus(),
+        tyomahdollisuus.getTiivistelma(),
+        tyomahdollisuus.getAmmattiryhma(),
+        tyomahdollisuus.isAktiivinen());
+  }
+}
