329bfbbdaa0aa091714ea425f116e3e98d99adf6
Lauri Holopainen <lauri.holopainen@elenium.fi>
2025-06-26 14:06:20 +0300

    OPHJOD-1698: Add endpoint for koulutusmahdollisuudet

--- src/main/java/fi/okm/jod/yksilo/externalapi/v1/dto/ExtAPIV1Mapper.java
+++ src/main/java/fi/okm/jod/yksilo/externalapi/v1/dto/ExtAPIV1Mapper.java
@@ -9,10 +9,12 @@
 
 package fi.okm.jod.yksilo.externalapi.v1.dto;
 
+import fi.okm.jod.yksilo.entity.koulutusmahdollisuus.Koulutusmahdollisuus;
 import fi.okm.jod.yksilo.entity.tyomahdollisuus.Tyomahdollisuus;
 
-public class ExtTyomahdollisuusMapper {
-  private ExtTyomahdollisuusMapper() {}
+/** */
+public class ExtAPIV1Mapper {
+  private ExtAPIV1Mapper() {}
 
   public static ExtTyoMahdollisuusDto toTyoMahdollisuusDto(final Tyomahdollisuus tyomahdollisuus) {
     return new ExtTyoMahdollisuusDto(
@@ -22,5 +24,16 @@ public class ExtTyomahdollisuusMapper {
         tyomahdollisuus.getTiivistelma(),
         tyomahdollisuus.getAmmattiryhma(),
         tyomahdollisuus.isAktiivinen());
+  }
+
+  public static ExtKoulutusMahdollisuusDto toKoulutusMahdollisuusDto(
+      Koulutusmahdollisuus koulutusmahdollisuus) {
+    return new ExtKoulutusMahdollisuusDto(
+        koulutusmahdollisuus.getId(),
+        koulutusmahdollisuus.getOtsikko(),
+        koulutusmahdollisuus.getTiivistelma(),
+        koulutusmahdollisuus.getKuvaus(),
+        koulutusmahdollisuus.getKesto(),
+        koulutusmahdollisuus.isAktiivinen());
   }
 }
---
+++ src/main/java/fi/okm/jod/yksilo/externalapi/v1/dto/ExtKoulutusMahdollisuusDto.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025 The Finnish Ministry of Education and Culture, The Finnish
+ * The Ministry of Economic Affairs and Employment, The Finnish National Agency of
+ * Education (Opetushallitus) and The Finnish Development and Administration centre
+ * for ELY Centres and TE Offices (KEHA).
+ *
+ * Licensed under the EUPL-1.2-or-later.
+ */
+
+package fi.okm.jod.yksilo.externalapi.v1.dto;
+
+import fi.okm.jod.yksilo.domain.LocalizedString;
+import fi.okm.jod.yksilo.entity.koulutusmahdollisuus.Koulutusmahdollisuus;
+import java.util.UUID;
+
+/** Tiedot mitä koulutusmahdollisuudesta palautetaan ulkoisen rajapinnan v1-versiossa */
+public record ExtKoulutusMahdollisuusDto(
+    UUID id,
+    LocalizedString otsikko,
+    LocalizedString tiivistelma,
+    LocalizedString kuvaus,
+    Koulutusmahdollisuus.KestoJakauma kesto,
+    boolean aktiivinen) {}
