62a5265e9f7c7d39bfba5a4fc1d417b437cb5144
Lauri Holopainen <lauri.holopainen@elenium.fi>
2025-06-27 12:24:45 +0300

    OPHJOD-1698: Merged to origin/main

--- src/main/java/fi/okm/jod/yksilo/externalapi/v1/ExternalApiV1Service.java
+++ src/main/java/fi/okm/jod/yksilo/externalapi/v1/ExternalApiV1Service.java
@@ -41,6 +41,7 @@ public class ExternalApiV1Service {
         this.tyomahdollisuusRepository.findAll(pageable);
     final List<ExtTyoMahdollisuusDto> tyoMahdollisuusDtoList =
         tyomahdollisuusPage.stream().map(ExtAPIV1Mapper::toTyoMahdollisuusDto).toList();
+
     return new SivuDto<>(
         tyoMahdollisuusDtoList,
         tyomahdollisuusPage.getTotalElements(),
@@ -52,6 +53,7 @@ public class ExternalApiV1Service {
         this.koulutusmahdollisuusRepository.findAll(pageable);
     final List<ExtKoulutusMahdollisuusDto> koulutusMahdollisuusDtoList =
         koulutusmahdollisuusPage.stream().map(ExtAPIV1Mapper::toKoulutusMahdollisuusDto).toList();
+
     return new SivuDto<>(
         koulutusMahdollisuusDtoList,
         koulutusmahdollisuusPage.getTotalElements(),
