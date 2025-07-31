73d97a85233272ba26b6916e99a513b95008728c
Lauri Holopainen <lauri.holopainen@elenium.fi>
2025-07-31 13:58:37 +0300

    OPHJOD-1810: Use new codestyle practices

--- src/main/java/fi/okm/jod/yksilo/entity/Ammatti.java
+++ src/main/java/fi/okm/jod/yksilo/entity/Ammatti.java
@@ -11,10 +11,17 @@ package fi.okm.jod.yksilo.entity;
 
 import fi.okm.jod.yksilo.domain.Kieli;
 import fi.okm.jod.yksilo.domain.LocalizedString;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MapKeyEnumerated;
 import java.net.URI;
 import java.util.Map;
-import java.util.Set;
 import lombok.Getter;
 import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.Immutable;
@@ -37,13 +44,6 @@ public class Ammatti {
   @MapKeyEnumerated(EnumType.STRING)
   @BatchSize(size = 1000)
   private Map<Kieli, Kaannos> kaannos;
-
-  @ManyToMany(fetch = FetchType.LAZY)
-  @JoinTable(
-      name = "yksilo_ammatti_kiinnostukset",
-      joinColumns = @JoinColumn(name = "ammatti_kiinnostukset_id"),
-      inverseJoinColumns = @JoinColumn(name = "yksilo_id"))
-  private Set<Yksilo> kiinnostuneet;
 
   @Embeddable
   public record Kaannos(
