d88949bdcb5acedc5b75106511eb0752e389bb59
Lauri Holopainen <lauri.holopainen@elenium.fi>
2025-08-06 17:11:46 +0300

    OPHJOD-1696: Added integration tests

---
+++ src/test/java/fi/okm/jod/yksilo/controller/profiili/YksilonSuosikkiControllerIT.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2025 The Finnish Ministry of Education and Culture, The Finnish
+ * The Ministry of Economic Affairs and Employment, The Finnish National Agency of
+ * Education (Opetushallitus) and The Finnish Development and Administration centre
+ * for ELY Centres and TE Offices (KEHA).
+ *
+ * Licensed under the EUPL-1.2-or-later.
+ */
+
+package fi.okm.jod.yksilo.controller.profiili;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import fi.okm.jod.yksilo.IntegrationTest;
+import fi.okm.jod.yksilo.domain.SuosikkiTyyppi;
+import fi.okm.jod.yksilo.dto.profiili.SuosikkiDto;
+import fi.okm.jod.yksilo.entity.YksilonSuosikki;
+import fi.okm.jod.yksilo.repository.YksiloRepository;
+import fi.okm.jod.yksilo.repository.YksilonSuosikkiRepository;
+import java.time.Instant;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+/** Integrationtests for {@link YksilonSuosikkiController} */
+@AutoConfigureMockMvc
+class YksilonSuosikkiControllerIT extends IntegrationTest {
+
+  @Autowired private MockMvc mockMvc;
+
+  @Autowired private YksilonSuosikkiRepository suosikkiRepository;
+  @Autowired private YksiloRepository yksiloRepository;
+
+  @WithUserDetails("test")
+  @Sql(
+      scripts = {"/data/add-koulutusmahdollisuus.sql"},
+      executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+  @Sql(
+      scripts = {"/data/cleanup.sql"},
+      executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+  @Transactional
+  @Test
+  void yksilonSuosikkiAdd200() throws Exception {
+    UUID koulutusMahdollisuusId = UUID.fromString("f47ac10b-58cc-4372-a567-0e02b2c3d479");
+    SuosikkiDto newSuosikki =
+        new SuosikkiDto(null, koulutusMahdollisuusId, SuosikkiTyyppi.KOULUTUSMAHDOLLISUUS, null);
+    final MvcResult mvcResult =
+        this.mockMvc
+            .perform(
+                post("/api/profiili/suosikit")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(this.objectMapper.writeValueAsString(newSuosikki)))
+            .andExpect(status().isOk())
+            .andReturn();
+
+    // lisää koulutusmahdollisuus
+    final UUID addedId = getResponse(mvcResult, UUID.class);
+    final YksilonSuosikki addedSuosikki =
+        suosikkiRepository.findById(addedId).orElseThrow(RuntimeException::new);
+    final Instant yksiloLuotu = addedSuosikki.getYksilo().getLuotu();
+    final Instant yksiloMuokattu = addedSuosikki.getYksilo().getMuokattu();
+    assertEquals(
+        koulutusMahdollisuusId,
+        addedSuosikki.getKoulutusmahdollisuus().getId(),
+        "Suosikilla on sama koulutusmahdollisuus kuin dto:ssa");
+    assertTrue(yksiloMuokattu.isAfter(yksiloLuotu));
+    assertEquals(
+        yksiloMuokattu,
+        addedSuosikki.getLuotu(),
+        "Yksilön aikaleimaa tulee päivittää kun suosikki luodaan");
+  }
+}
---
+++ src/test/resources/data/osaaminen.sql
@@ -0,0 +1,10 @@
+INSERT INTO osaaminen(id, uri) VALUES
+                                 (10, 'urn:osaaminen1'),
+                                 (12, 'urn:osaaminen2'),
+                                 (13, 'urn:osaaminen3'),
+                                 (14, 'urn:osaaminen4'),
+                                 (101, 'urn:osaaminen5') ,
+                                 (121, 'urn:osaaminen6') ,
+                                 (131, 'urn:osaaminen7') ,
+                                 (141, 'urn:osaaminen8'),
+                                 (1011, 'urn:osaaminen9');
