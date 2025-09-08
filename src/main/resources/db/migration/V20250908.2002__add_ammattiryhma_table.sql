35c9ae9ccc39153ca1e2331b974c1268eb2cd7fc
Lauri Holopainen <lauri.holopainen@elenium.fi>
2025-09-08 12:32:39 +0300

    OPHJOD-2002: Add table for ammattiryhmat

---
+++ src/main/resources/db/migration/V20250908.2002__add_ammattiryhma_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE yksilo.ammattiryhma
+(
+  id                  BIGINT                 NOT NULL PRIMARY KEY ,
+  esco_uri            CHARACTER VARYING(255) NOT NULL,
+  mediaani_palkka     INT,
+  ylin_desiili_palkka INT,
+  alin_desiili_palkka INT
+);
+
+CREATE INDEX ar_esco_uri ON ammattiryhma (esco_uri);
+
