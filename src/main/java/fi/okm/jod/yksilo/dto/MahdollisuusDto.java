f41144dd7aaad9e612d5d6fc827521ef6e98f0ce
Lauri Holopainen <lauri.holopainen@elenium.fi>
2025-10-09 15:05:29 +0300

    OPHJOD-2495: Add aineisto to mahdollisuus metadata

--- src/main/java/fi/okm/jod/yksilo/dto/MahdollisuusDto.java
+++ src/main/java/fi/okm/jod/yksilo/dto/MahdollisuusDto.java
@@ -10,10 +10,19 @@
 package fi.okm.jod.yksilo.dto;
 
 import fi.okm.jod.yksilo.domain.MahdollisuusTyyppi;
+import fi.okm.jod.yksilo.domain.TyomahdollisuusAineisto;
 import java.util.UUID;
 
-public record MahdollisuusDto(UUID id, MahdollisuusTyyppi tyypi, String ammattiryhma) {
-  public MahdollisuusDto(UUID id, String tyyppi, String ammattiryhma) {
-    this(id, MahdollisuusTyyppi.valueOf(tyyppi), ammattiryhma);
+public record MahdollisuusDto(
+    UUID id, MahdollisuusTyyppi tyypi, String ammattiryhma, TyomahdollisuusAineisto aineisto) {
+  public MahdollisuusDto(UUID id, String tyyppi, String ammattiryhma, String aineisto) {
+
+    this(
+        id,
+        MahdollisuusTyyppi.valueOf(tyyppi),
+        ammattiryhma,
+        (aineisto == null || aineisto.isEmpty())
+            ? null
+            : TyomahdollisuusAineisto.valueOf(aineisto));
   }
 }
