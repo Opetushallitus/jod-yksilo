7e2dd884ed390154c378b5a2e1899d6edf391f6c
Lauri Holopainen <lauri.holopainen@elenium.fi>
2025-10-23 14:30:51 +0300

    OPHJOD-2383: Add yleisinKoulutusala to koulutusmahdollisuus response

--- src/main/java/fi/okm/jod/yksilo/dto/KoulutusmahdollisuusDto.java
+++ src/main/java/fi/okm/jod/yksilo/dto/KoulutusmahdollisuusDto.java
@@ -21,4 +21,5 @@ public record KoulutusmahdollisuusDto(
     LocalizedString tiivistelma,
     LocalizedString kuvaus,
     KestoJakaumaDto kesto,
+    String yleisinKoulutusala,
     Boolean aktiivinen) {}
