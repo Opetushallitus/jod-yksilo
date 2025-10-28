0a0a490462620ffe1af40c72515d5c546f68a3fd
Lauri Holopainen <lauri.holopainen@elenium.fi>
2025-10-28 12:10:30 +0200

    OPHJOD-2609: Add reasonable constraint-names and remove unnecessary changes

--- src/main/resources/db/migration/V20251027.2609__paamaara_to_tavoite.sql
+++ src/main/resources/db/migration/V20251027.2609__paamaara_to_tavoite.sql
@@ -4,14 +4,14 @@ ALTER TABLE paamaara_kaannos RENAME TO tavoite_kaannos;
 ALTER TABLE tavoite_kaannos DROP CONSTRAINT fkpscq7acrnhqruqeitw7puana1;
 ALTER TABLE tavoite_kaannos RENAME COLUMN paamaara_id TO tavoite_id;
 ALTER TABLE tavoite_kaannos
-  ADD CONSTRAINT constraint_name
+  ADD CONSTRAINT fk_tk_tavoite_id
     FOREIGN KEY (tavoite_id)
       REFERENCES tavoite (id);
 
 ALTER TABLE polun_suunnitelma DROP CONSTRAINT fk7td9koaembgy6o5su5xgk0ch4;
 ALTER TABLE polun_suunnitelma RENAME COLUMN paamaara_id TO tavoite_id;
 ALTER TABLE polun_suunnitelma
-  ADD CONSTRAINT constraint_name
+  ADD CONSTRAINT fk_ps_tavoite_id
     FOREIGN KEY (tavoite_id)
       REFERENCES tavoite (id);
 
