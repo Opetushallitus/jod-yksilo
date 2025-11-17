ad522e8c407c2aab4605eb39e491fd720978281e
Lauri Holopainen <lauri.holopainen@elenium.fi>
2025-11-17 19:53:28 +0200

    OPHJOD-2609: Add missing migration

---
+++ src/main/resources/db/migration/V20251117.2609__tavoite_related_changes.sql
@@ -0,0 +1,22 @@
+ALTER TABLE tavoite_kaannos
+  ADD COLUMN IF NOT EXISTS kuvaus TEXT;
+
+ALTER TABLE tavoite
+  DROP CONSTRAINT IF EXISTS paamaara_tyyppi_check;
+ALTER TABLE tavoite
+  DROP CONSTRAINT IF EXISTS paamaara_check;
+
+ALTER TABLE polun_suunnitelma
+  ADD COLUMN IF NOT EXISTS koulutusmahdollisuus_id UUID;
+ALTER TABLE polun_suunnitelma
+  ADD CONSTRAINT fk_ps_koulutusmahdollisuus
+    FOREIGN KEY (koulutusmahdollisuus_id) REFERENCES koulutusmahdollisuus (id);
+
+ALTER TABLE polun_suunnitelma_kaannos
+  ADD COLUMN IF NOT EXISTS kuvaus TEXT;
+
+DROP TABLE IF EXISTS polun_vaihe CASCADE;
+DROP TABLE IF EXISTS polun_vaihe_kaannos CASCADE;
+DROP TABLE IF EXISTS polun_vaihe_linkit CASCADE;
+DROP TABLE IF EXISTS polun_vaihe_osaamiset CASCADE;
+DROP TABLE IF EXISTS polun_suunnitelma_ignored_osaamiset;
