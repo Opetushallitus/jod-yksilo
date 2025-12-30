3b63bfe6663f2ddc9f4582b9924a6027fe029d3a
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2025-12-30 08:22:01 +0200

    OPHJOD-2871: Update ammattiryhma data

--- scripts/devdata/ammattiryhma.sql
+++ scripts/devdata/ammattiryhma.sql
@@ -7,4 +7,4 @@ COPY ammattiryhma_import (esco_uri, data) FROM STDIN CSV HEADER;
 INSERT INTO ammattiryhma (esco_uri, data)
 SELECT esco_uri, data
 FROM ammattiryhma_import
-ON CONFLICT (esco_uri) DO UPDATE SET data = EXCLUDED.data;
+ON CONFLICT (esco_uri) DO UPDATE SET data = EXCLUDED.data, muokattu = now();
