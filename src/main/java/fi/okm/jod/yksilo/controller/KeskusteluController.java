1d773edc101c06cb7e6750d04ad096fd169c926a
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2026-02-11 09:00:16 +0200

    OPHJOD-3078: Update virtual assistant API
    - Add mode parameter

--- src/main/java/fi/okm/jod/yksilo/controller/KeskusteluController.java
+++ src/main/java/fi/okm/jod/yksilo/controller/KeskusteluController.java
@@ -12,16 +12,20 @@ package fi.okm.jod.yksilo.controller;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import fi.okm.jod.yksilo.config.feature.Feature;
 import fi.okm.jod.yksilo.config.feature.FeatureRequired;
+import fi.okm.jod.yksilo.domain.Kieli;
 import fi.okm.jod.yksilo.domain.LocalizedString;
 import fi.okm.jod.yksilo.service.inference.InferenceService;
-import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.net.URI;
 import java.time.Instant;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
@@ -56,19 +60,13 @@ public class KeskusteluController {
   }
 
   @PostMapping
-  @Operation(summary = "Creates a keskustelu")
   public Keskustelu createKeskustelu(
-      @RequestBody @NotNull @Size(min = 2, max = 10_000) LocalizedString kuvaus,
-      HttpServletRequest request) {
-    if (kuvaus.asMap().size() > 1) {
-      throw new IllegalArgumentException("Ambiguous request, only one language version allowed");
-    }
+      @RequestBody @NotNull @Valid UusiKeskustelu uusiKeskustelu, HttpServletRequest request) {
 
-    var entry = kuvaus.asMap().entrySet().iterator().next();
     var inferenceResponse =
         inferenceService.infer(
             endpoint,
-            new InferenceRequest(null, entry.getValue(), entry.getKey().toString()),
+            InferenceRequest.newConversation(uusiKeskustelu.viesti(), uusiKeskustelu.tila()),
             new ParameterizedTypeReference<>() {});
 
     var httpSession = request.getSession();
@@ -78,15 +76,14 @@ public class KeskusteluController {
 
     return new Keskustelu(
         inferenceResponse.session().id(),
-        inferenceResponse.kiinnostukset(),
-        inferenceResponse.response());
+        inferenceResponse.response(),
+        inferenceResponse.suggestions());
   }
 
   @PostMapping("/{id}")
-  @Operation(summary = "Continues a keskustelu")
   public ResponseEntity<Vastaus> continueKeskustelu(
       @PathVariable UUID id,
-      @RequestBody @NotNull @Size(min = 2, max = 10_000) LocalizedString kuvaus,
+      @RequestBody @NotNull @Size(min = 2, max = 1000) LocalizedString viesti,
       HttpServletRequest request) {
 
     if (!(request.getSession(false) instanceof HttpSession httpSession
@@ -105,37 +102,71 @@ public class KeskusteluController {
       return ResponseEntity.notFound().build();
     }
 
-    var entries = kuvaus.asMap().entrySet();
-    if (entries.size() > 1) {
-      throw new IllegalArgumentException("Ambiguous request, only one language version allowed");
-    }
-
     log.info("Continuing conversation {}", id);
-    var entry = entries.iterator().next();
     var inferenceResponse =
         inferenceService.infer(
             endpoint,
-            new InferenceRequest(inferenceSession, entry.getValue(), entry.getKey().toString()),
+            InferenceRequest.continueConversation(inferenceSession, viesti),
             RESPONSE_TYPE);
     return ResponseEntity.ok(
-        new Vastaus(inferenceResponse.kiinnostukset(), inferenceResponse.response()));
+        new Vastaus(inferenceResponse.response(), inferenceResponse.suggestions()));
+  }
+
+  public enum Mode {
+    INTERESTS,
+    SKILLS
+  }
+
+  public enum Tila {
+    KIINNOSTUKSET,
+    OSAAMINEN
   }
 
   public record InferenceRequest(
       InferenceSession session,
       String message,
-      @JsonProperty("language_code") String languageCode) {}
+      @JsonProperty("language_code") String languageCode,
+      Mode mode) {
+
+    static InferenceRequest newConversation(LocalizedString message, Tila tila) {
+      var entry = validate(message);
+      return new InferenceRequest(
+          null,
+          entry.getValue(),
+          entry.getKey().getKoodi(),
+          Tila.OSAAMINEN.equals(tila) ? Mode.SKILLS : Mode.INTERESTS);
+    }
+
+    static InferenceRequest continueConversation(
+        InferenceSession session, LocalizedString message) {
+      var entry = validate(message);
+      return new InferenceRequest(session, entry.getValue(), entry.getKey().getKoodi(), null);
+    }
+
+    private static Entry<Kieli, String> validate(LocalizedString message) {
+      var entries = message.asMap().entrySet();
+      if (entries.size() > 1) {
+        throw new IllegalArgumentException("Ambiguous request, only one language version allowed");
+      }
+      return entries.iterator().next();
+    }
+  }
 
   public record InferenceResponse(
-      InferenceSession session, Set<Kiinnostus> kiinnostukset, String response) {}
+      InferenceSession session, Set<URI> suggestions, String response) {}
 
   public record InferenceSession(UUID id, long timestamp, String signature) {}
 
-  public record Keskustelu(UUID id, Set<Kiinnostus> kiinnostukset, String vastaus) {}
+  public record UusiKeskustelu(
+      @NotNull @Size(min = 2, max = 1000) LocalizedString viesti, @NotNull Tila tila) {}
 
-  public record Vastaus(Set<Kiinnostus> kiinnostukset, String vastaus) {}
+  public record Keskustelu(
+      @Schema(accessMode = AccessMode.READ_ONLY) UUID id,
+      @NotNull String vastaus,
+      Set<URI> ehdotukset) {}
 
-  public record Kiinnostus(@JsonProperty("esco_uri") URI escoUri, String kuvaus) {}
+  public record Vastaus(
+      @Schema(accessMode = AccessMode.READ_ONLY) @NotNull String vastaus, Set<URI> ehdotukset) {}
 
   private static final ParameterizedTypeReference<InferenceResponse> RESPONSE_TYPE =
       new ParameterizedTypeReference<>() {};
