59a5bbe5f53d6daa394788bcd582b2f2b3367c18
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2026-02-23 07:22:13 +0200

    OPHJOD-3171: Optimize mahdollisuus search
    - Add trigram index to speed up %like% queries.
    - Utilize cached mahdollisuus data in search results

--- scripts/docker-entrypoint-initdb.d/01-init-db.sh
+++ scripts/docker-entrypoint-initdb.d/01-init-db.sh
@@ -8,6 +8,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<'E
 EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "yksilo" <<'EOSQL'
+    CREATE EXTENSION IF NOT EXISTS "pg_trgm";
+
     BEGIN;
     CREATE SCHEMA IF NOT EXISTS yksilo;
     GRANT USAGE ON SCHEMA yksilo TO dataloader;
---
+++ src/test/resources/db/setup.sql
@@ -0,0 +1,2 @@
+-- initial setup for the test database, runs before migrations
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
